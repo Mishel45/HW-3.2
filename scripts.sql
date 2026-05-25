9e7e8942276a16a57383dcaa5ca526f4a363054a
Mishel45 <mikhail-pavlov1980@mail.ru>
2026-05-25 22:41:19 +0300

    3.3 finish

---
+++ scripts.sql
@@ -0,0 +1,6 @@
+SELECT * FROM student;
+SELECT * FROM student WHERE age BETWEEN 11 AND 15;
+SELECT name FROM student;
+SELECT * FROM student WHERE name ILIKE '%о%';
+SELECT * FROM student WHERE age < id;
+SELECT * FROM student ORDER BY age;
