b2ddf50b9bc7aa17d75dae2dbdb4516d6129e338
Mishel45 <mikhail-pavlov1980@mail.ru>
2026-09-07 23:57:45 +0300

    Create scripts422.sql

---
+++ scripts422.sql
@@ -0,0 +1,16 @@
+CREATE TABLE car (
+    id BIGSERIAL PRIMARY KEY,
+    brand VARCHAR(100) NOT NULL,
+    model VARCHAR(100) NOT NULL,
+    price NUMERIC(12, 2) NOT NULL
+);
+
+CREATE TABLE person (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(150) NOT NULL,
+    age INT CHECK (age >= 0),
+    has_driver_license BOOLEAN NOT NULL DEFAULT FALSE,
+    car_id BIGINT,
+    CONSTRAINT fk_person_car FOREIGN KEY (car_id) REFERENCES car(id) ON DELETE SET NULL
+);
+
